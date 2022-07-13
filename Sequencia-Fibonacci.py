cba7cd499f2d866ad0d9a02a80b82efb6f28ca6a
Ruan T Ferreira <83648659+Condor0@users.noreply.github.com>
2022-07-13 11:56:54 -0300

    Add files via upload

---
+++ Sequencia-Fibonacci.py
@@ -0,0 +1,8 @@
+sequencia = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+x = int(input("Digite um número entre 1 e 89: "))
+
+if x in sequencia:
+    print("Contem na sequencia.")
+else:
+    print("o numero nao esta na sequencia.")
